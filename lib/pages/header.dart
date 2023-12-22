8290b58265af060b1467bb93659274531080ba19
AHMED <hossainahmad76@gmail.com>
2023-12-22 21:16:27 +0600

    Mobile, Tab & WebView

--- lib/pages/header.dart
+++ lib/pages/header.dart
@@ -2,13 +2,19 @@ import 'package:flutter/material.dart';
 
 import '../style.dart';
 
-class Header extends StatelessWidget {
+class Header extends StatefulWidget {
   const Header({
     super.key,
   });
 
   @override
+  State<Header> createState() => _HeaderState();
+}
+
+class _HeaderState extends State<Header> {
+  @override
   Widget build(BuildContext context) {
+    String _selectedValue = '';
     return LayoutBuilder(
         builder: (BuildContext context, BoxConstraints constraints) {
       if (constraints.maxWidth < 450) {
@@ -20,17 +26,34 @@ class Header extends StatelessWidget {
               'H.',
               style: TextStyle(fontSize: 45, color: whiteColor),
             ),
-            Row(
-              children: [
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(
-                    Icons.menu,
-                    color: Colors.white,
-                  ),
+            PopupMenuButton<String>(
+                icon: const Icon(
+                  Icons.menu,
+                  color: whiteColor,
                 ),
-              ],
-            ),
+                onSelected: (String value) {
+                  setState(() {
+                    _selectedValue = value;
+                  });
+                },
+                itemBuilder: (BuildContext context) => [
+                      const PopupMenuItem(
+                        value: '1',
+                        child: Text('MY INTRO'),
+                      ),
+                      const PopupMenuItem(
+                        value: '2',
+                        child: Text('SERVICES'),
+                      ),
+                      const PopupMenuItem(
+                        value: '3',
+                        child: Text('PROJECTS'),
+                      ),
+                      const PopupMenuItem(
+                        value: '3',
+                        child: Text('BLOGS'),
+                      ),
+                    ]),
           ],
         );
       } else if (constraints.maxWidth < 750) {
